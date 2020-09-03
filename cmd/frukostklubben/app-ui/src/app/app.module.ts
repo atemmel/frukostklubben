2ca9a4d2297bbeb6e7ed154a4d0ad2c944670cc8
Oscar Fredriksson <osfr1701@student.miun.se>
2020-09-03 17:54:22 +0200

    start of basic gui

--- cmd/frukostklubben/app-ui/src/app/app.module.ts
+++ cmd/frukostklubben/app-ui/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MaterialModule } from './material.module';
+import { ChatMessageComponent } from './components/chat-message/chat-message.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { UserSectionComponent } from './components/user-section/user-section.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ChatMessageComponent, ChatComponent, UserSectionComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- cmd/frukostklubben/app-ui/src/app/material.module.ts
+++ cmd/frukostklubben/app-ui/src/app/material.module.ts
@@ -1,9 +1,26 @@
 import { NgModule } from '@angular/core';
 
 import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+
+import { MatIconModule } from '@angular/material/icon';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @NgModule({
-  imports: [MatCardModule],
-  exports: [MatCardModule],
+  imports: [
+    MatCardModule,
+    MatGridListModule,
+    MatInputModule,
+    MatIconModule,
+    ScrollingModule,
+  ],
+  exports: [
+    MatCardModule,
+    MatGridListModule,
+    MatInputModule,
+    MatIconModule,
+    ScrollingModule,
+  ],
 })
 export class MaterialModule {}
