7a3e02572f4a8fca8d33101f300d1b4fb7575814
iSellgren <frse1700@student.miun.se>
2020-09-05 15:03:08 +0200

    Fixade spinning Perreli för users

--- cmd/frukostklubben/app-ui/src/app/app.module.ts
+++ cmd/frukostklubben/app-ui/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MaterialModule } from './material.module';
 import { ChatMessageComponent } from './components/chat-message/chat-message.component';
 import { ChatComponent } from './components/chat/chat.component';
 import { UserSectionComponent } from './components/user-section/user-section.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, ChatMessageComponent, ChatComponent, UserSectionComponent],
@@ -17,6 +18,7 @@ import { UserSectionComponent } from './components/user-section/user-section.com
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
