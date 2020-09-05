8735169a1c052b8caf62e777b74ea1e8927b9df2
Oscar Fredriksson <osfr1701@student.miun.se>
2020-09-05 15:03:48 +0200

    added user is typing text and formula font

--- cmd/frukostklubben/app-ui/src/app/app.module.ts
+++ cmd/frukostklubben/app-ui/src/app/app.module.ts
@@ -4,19 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { MaterialModule } from './material.module';
 import { ChatMessageComponent } from './components/chat-message/chat-message.component';
 import { ChatComponent } from './components/chat/chat.component';
 import { UserSectionComponent } from './components/user-section/user-section.component';
+import { UserTypingComponent } from './components/user-typing/user-typing.component';
 
 @NgModule({
-  declarations: [AppComponent, ChatMessageComponent, ChatComponent, UserSectionComponent],
+  declarations: [
+    AppComponent,
+    ChatMessageComponent,
+    ChatComponent,
+    UserSectionComponent,
+    UserTypingComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- cmd/frukostklubben/app-ui/src/app/components/chat-message/chat-message.component.ts
+++ cmd/frukostklubben/app-ui/src/app/components/chat-message/chat-message.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import {trigger, transition, animate, style} from '@angular/animations';
+import { trigger, transition, animate, style } from '@angular/animations';
 export interface ChatMessage {
   author?: string;
   timestamp: string;
@@ -10,15 +10,18 @@ export interface ChatMessage {
   selector: 'app-chat-message',
   templateUrl: './chat-message.component.html',
   styleUrls: ['./chat-message.component.scss'],
-  animations: [trigger('entryAnimation', [
+  animations: [
+    trigger('entryAnimation', [
       transition(':enter', [
-        style({ opacity: 0, transform: 'scale(0)','transform-origin': '0 0'}),
-        animate('1000ms', style({ opacity: 1,transform: 'scale(1) translateY(0%)' })),
-      ])
-    ])
-  ]
-  })
-  
+        style({ opacity: 0, transform: 'scale(0)', 'transform-origin': '0 0' }),
+        animate(
+          '1000ms',
+          style({ opacity: 1, transform: 'scale(1) translateY(0%)' })
+        ),
+      ]),
+    ]),
+  ],
+})
 export class ChatMessageComponent implements OnInit {
   @Input() message: ChatMessage;
 
