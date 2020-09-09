db0a6ea0dfa9f6bf7633986ee17e4dafe5cf75b8
Oscar Fredriksson <osfr1701@student.miun.se>
2020-09-09 10:14:35 +0200

    fixed broken code from adding splashscreen :)

---
+++ cmd/frukostklubben/app-ui/src/app/services/p2p.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { ChatMessage } from '../components/chat-message/chat-message.component';
+
+declare var astilectron: any;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class P2pService {
+  public ready: boolean = false;
+
+  public readyCallback: () => void;
+
+  //public onMessage: (message: ChatMessage) => void;
+
+  constructor() {
+    document.addEventListener('astilectron-ready', () => {
+      console.log('ready...');
+
+      this.ready = true;
+      if (this.readyCallback) this.readyCallback();
+    });
+  }
+
+  public onMessage(callback: (message: ChatMessage) => void) {
+    astilectron.onMessage(callback);
+  }
+
+  public sendMessage(message: ChatMessage) {
+    astilectron.sendMessage(message);
+  }
+}
