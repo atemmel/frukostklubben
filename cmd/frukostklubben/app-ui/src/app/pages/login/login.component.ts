62045c01f4d052ed8c585fcc1780c3dc70928062
iSellgren <frse1700@student.miun.se>
2020-09-09 15:31:22 +0200

    Fixade colorpicker för användaren

--- cmd/frukostklubben/app-ui/src/app/pages/login/login.component.ts
+++ cmd/frukostklubben/app-ui/src/app/pages/login/login.component.ts
@@ -3,6 +3,7 @@ import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Route } from '@angular/compiler/src/core';
 import { Router } from '@angular/router';
+import { EventEmitter, Input, Output } from '@angular/core';
 
 import {
   FormBuilder,
@@ -18,6 +19,43 @@ import { P2pService } from 'src/app/services/p2p.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+
+  @Input() heading: string;
+  @Input() color: string;
+  @Output() event = new EventEmitter();
+  public show = false;
+  public defaultColors: string[] = [
+    '#ffffff',
+    '#FFFF00',
+    '#3e6158',
+    '#3f7a89',
+    '#96c582',
+    '#b7d5c4',
+    '#bcd6e7',
+    '#7c90c1',
+    '#9d8594',
+    '#dad0d8',
+    '#4b4fce',
+    '#4e0a77',
+    '#a367b5',
+    '#ee3e6d',
+    '#d63d62',
+    '#c6a670',
+    '#f46600',
+    '#cf0500',
+    '#efabbd',
+    '#8e0622',
+    '#f0b89a',
+    '#f0ca68',
+    '#62382f',
+    '#c97545',
+    '#c1800b'
+  ];
+
+
+
+
+
   loginForm: FormGroup;
 
   constructor(
@@ -28,6 +66,28 @@ export class LoginComponent implements OnInit {
     this.loginForm = this.formBuilder.group({
       username: new FormControl('', [Validators.required]),
     });
+  }
+  /**
+   * Change color from default colors
+   * @param {string} color
+   */
+  public changeColor(color: string): void {
+    this.color = color;
+    this.event.emit(this.color);
+    this.show = false;
+  }
+
+
+  /**
+   * Change color from input
+   * @param {string} color
+   */
+
+  /**
+   * Change status of visibility to color picker
+   */
+  public toggleColors(): void {
+    this.show = !this.show;
   }
 
   ngOnInit(): void {}
